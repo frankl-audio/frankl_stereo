a6cea853ea9e5f47272d5459138e94b57b7c2724
frankl <frank@luebecknet.de>
2024-01-04 22:59:53 +0100

    highrestest: add precision test
    E.g.,
       highrestest 500
    will test with 10000 sleeps in 10 seconds the accuracy of
    the wake up time and do some statistics in 500ns intervals.

--- src/highrestest.c
+++ src/highrestest.c
@@ -1,5 +1,5 @@
 /*
-highrestest.c                Copyright frankl 2013-2014
+highrestest.c                Copyright frankl 2013-2024
 
 This file is part of frankl's stereo utilities. 
 See the file License.txt of the distribution and 
@@ -7,11 +7,24 @@ http://www.gnu.org/licenses/gpl.txt for license details.
 */
 #include <time.h>
 #include <stdio.h>
+#include <stdlib.h>
 
+//#define MYCLOCK CLOCK_MONOTONIC_RAW
+#define MYCLOCK CLOCK_MONOTONIC
+
+/* t2 - t1 in nanoseconds */
+long difftimens(struct timespec t1, struct timespec t2)
+{ 
+   long long l1, l2;
+   l1 = t1.tv_sec*1000000000 + t1.tv_nsec;
+   l2 = t2.tv_sec*1000000000 + t2.tv_nsec;
+   return (long)(l2-l1);
+}
 /* a simple test of the resolution of several CLOCKs */
-int main() {
-  int ret, highresok;
-  struct timespec res, tim;
+int main(int argc, char *argv[]) {
+  int ret, highresok, first, nloops, i, k;
+  long step, d, min, max, dev, dint, count[21];
+  struct timespec res, last, tim;
 
   printf("----- Testing highres timer:\n");
 
@@ -33,6 +46,65 @@ int main() {
 
   ret = clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &tim);
   printf("process: %ld s %ld ns (%d)\n", tim.tv_sec, tim.tv_nsec, ret); 
+
+  if (highresok && argc > 1) {
+    printf("Measuring actual precision of monotonic clock for 10 seconds ...\n");
+    step = 1000000;
+    nloops = 10000;
+    if (argc > 1)
+      dint = atoi(argv[1]);
+    else
+      dint = 500;
+    min = 0;
+    max = 0;
+    dev =0;
+    clock_gettime(MYCLOCK, &res);
+    last = res;
+    
+    for(i=0; i<21; count[i]=0, i++);
+    /* avoid some startup jitter */
+    for(first=100, i=0; i < nloops+99; i++) 
+    {
+      res.tv_nsec = res.tv_nsec+step;
+      if (res.tv_nsec > 999999999) {
+        res.tv_sec++;
+        res.tv_nsec -= 1000000000;
+      }
+      clock_nanosleep(MYCLOCK, TIMER_ABSTIME, &res, NULL);
+      clock_gettime(MYCLOCK, &tim);
+      d = difftimens(last, tim)-step;
+      k = d/dint;
+      if (k < -10) k = -10;
+      if (k > 10) k = 10;
+      count[k+10]++;
+      last = tim;
+
+      //printf("%ld\n", d);
+      if (first == 0) {
+        if (d < min)
+           min = d;
+        if (d > max)
+           max = d;
+        dev += d;
+      } else if (first == 1) {
+        min = d;
+        max = d;
+        first = 0;
+      } else 
+        first--;
+    }
+    printf("    Min diff: %ld ns, max diff: %ld ns, \n"
+           "    avg. diff: %ld ns\n",
+           min, max, dev/nloops);
+    printf("   diff in ns      count\n");
+    printf(" < -10*%ld        %ld\n", dint, count[0]);
+    for(i=-9; i<10; i++)
+        printf("    %d*%ld        %ld\n", i, dint, count[i+10]);
+    printf(" >  10*%ld        %ld\n", dint, count[20]);
+
+  }
+
+      
 
   if (highresok) 
      printf("\nHighres timer seems enabled!\n");
